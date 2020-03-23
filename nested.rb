99cd3ec39245bd419ad675ad8c75b226471d47b1
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-23 05:16:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yoki] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  return programmer_hash
 end
 
 def changing_alan
